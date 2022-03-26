26f3c2d55e091cc4e72f60f36e3e32259ba38aad
az <azzayan@gmail.com>
2022-03-26 09:14:46 -0500

    Making a small change to test.

--- src/components/MainLayout/MainHeader.js
+++ src/components/MainLayout/MainHeader.js
@@ -1,19 +1,17 @@
-import React, {PropTypes} from 'react';
-import {AppBar} from 'react-toolbox/lib/app_bar';
-import {IconButton} from 'react-toolbox/lib/button';
-import style from './mainLayout.scss';
+import React, { PropTypes } from "react";
+import { AppBar } from "react-toolbox/lib/app_bar";
+import { IconButton } from "react-toolbox/lib/button";
+import style from "./mainLayout.scss";
 
-const MainHeader = (props)=> {
-    return (
-        <AppBar>
-            <IconButton icon='menu' inverse={ true } onClick={ props.onToggleSideBar }/>
-            <div className={style.appBarCenterGrow}>
-                {"Welcome to the World of AZ"}
-            </div>
-        </AppBar>
-    );
+const MainHeader = (props) => {
+  return (
+    <AppBar>
+      <IconButton icon="menu" inverse={true} onClick={props.onToggleSideBar} />
+      <div className={style.appBarCenterGrow}>{"Welcome!"}</div>
+    </AppBar>
+  );
 };
 MainHeader.propTypes = {
-    onToggleSideBar: PropTypes.func.isRequired
+  onToggleSideBar: PropTypes.func.isRequired,
 };
 export default MainHeader;
